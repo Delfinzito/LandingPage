eda5ac2162499012ed992ee960309aabba0c90b3
Delfinzito <146035305+Delfinzito@users.noreply.github.com>
2023-10-11 17:47:09 -0300

    Add files via upload

---
+++ js/dadsa.js
@@ -0,0 +1,6 @@
+// O JavaScript não é estritamente necessário para fazer o link pulsar, mas pode ser usado para adicionar comportamentos extras quando o link é clicado.
+document.getElementById("pulsating-link").addEventListener("click", function() {
+    // Adicione aqui qualquer ação que você deseja que ocorra quando o link é clicado.
+    alert("Você clicou no link pulsante!");
+  });
+  
